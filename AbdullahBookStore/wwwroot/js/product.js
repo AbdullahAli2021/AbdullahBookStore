64b078aa70726e3ea8b78b1a7e6cac927c2874f5
Abdulah Ali <w0782917@myscc.ca>
2021-12-05 21:34:27 -0500

    fixing errorr in product view and javascript

--- AbdullahBookStore/wwwroot/js/product.js
+++ AbdullahBookStore/wwwroot/js/product.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/Admin/Product/GetAll"
+            "url": "/Admin/Product/GetAll/"
         },
         "columns": [
             { "data": "title", "width": "15%" },
             { "data": "isbn", "width": "15%" },
-            { "data": "price", "width": "15%" },
+            { "data": "listPrice", "width": "15%" },
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "15%" },
             {
